7baa4425db904aafc5681f8e942976f0ba4f6d5c
williamvietnguyen <williamvnguyen2@gmail.com>
2020-10-21 18:15:47 -0700

    More

---
+++ binary_trees/101_symmetric.py
@@ -0,0 +1,42 @@
+"""
+https://leetcode.com/problems/symmetric-tree/
+
+Given a binary tree, check whether it is a mirror of 
+itself (ie, symmetric around its center).
+
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+ 
+
+But the following [1,2,2,null,3,null,3] is not:
+    1
+   / \
+  2   2
+   \   \
+   3    3
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        def traversal(node1, node2):
+            if node1 is None and node2 is None:
+                return True
+            elif node1 is None or node2 is None:
+                return False
+            else:
+                return node1.val == node2.val and traversal(node1.left, node2.right) and traversal(node1.right, node2.left)
+        if root is None:
+            return True
+        return traversal(root.left, root.right)
---
+++ binary_trees/112_path_sum.py
@@ -0,0 +1,43 @@
+"""
+https://leetcode.com/problems/path-sum/
+
+Given a binary tree and a sum, determine if the tree has 
+a root-to-leaf path such that adding up all the values along 
+the path equals the given sum.
+
+Note: A leaf is a node with no children.
+
+Example:
+Given the below binary tree and sum = 22,
+      5
+     / \
+    4   8
+   /   / \
+  11  13  4
+ /  \      \
+7    2      1
+return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def hasPathSum(self, root: TreeNode, target: int) -> bool:
+        def traversal(node, s):
+            if node is None:
+                return False
+            s -= node.val
+            # leaf
+            if node.left is None and node.right is None and s == 0:
+                return True
+            return traversal(node.left, s) or traversal(node.right, s)
+
+        if root is None:
+            return False
+        return traversal(root, target)
